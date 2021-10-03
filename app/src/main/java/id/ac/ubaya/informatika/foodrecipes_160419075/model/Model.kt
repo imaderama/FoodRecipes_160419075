b58e4fe163c23c5a45d0bd16e878bde6917a6d13
imaderama <s160419075@student.ubaya.ac.id>
2021-10-03 17:13:02 +0800

    add Model.kt and RecipeListAdapter.kt

---
+++ app/src/main/java/id/ac/ubaya/informatika/foodrecipes_160419075/model/Model.kt
@@ -0,0 +1,9 @@
+package id.ac.ubaya.informatika.foodrecipes_160419075.model
+
+data class Recipe(
+    val id:Int?,
+    val name:String?,
+    val category: String?,
+    val likes:Int?,
+    val poster:String?
+)
---
+++ app/src/main/java/id/ac/ubaya/informatika/foodrecipes_160419075/view/RecipeListAdapter.kt
@@ -0,0 +1,37 @@
+package id.ac.ubaya.informatika.foodrecipes_160419075.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import id.ac.ubaya.informatika.foodrecipes_160419075.R
+import id.ac.ubaya.informatika.foodrecipes_160419075.model.Recipe
+import kotlinx.android.synthetic.main.recipe_list_item.view.*
+
+class RecipeListAdapter(val recipeList:ArrayList<Recipe>):RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder>() {
+    class RecipeViewHolder(val view: View):RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val v = inflater.inflate(R.layout.recipe_list_item, parent, false)
+
+        return RecipeViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        with(holder.view){
+            txtNama.text = recipeList[position].name
+            txtCategory.text = recipeList[position].category
+
+            btnDetails.setOnClickListener {
+//                val action = RecipeListFragmentDirections.action
+//                Navigation.findNavController(it).navigate(action)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return recipeList.size
+    }
+}
