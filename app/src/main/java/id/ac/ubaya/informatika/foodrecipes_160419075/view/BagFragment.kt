1206a4ceee0371aad628d654ab37cb47c4b90d63
imaderama <s160419075@student.ubaya.ac.id>
2021-10-07 21:25:37 +0800

    add BagFragment.kt

---
+++ app/src/main/java/id/ac/ubaya/informatika/foodrecipes_160419075/view/BagFragment.kt
@@ -0,0 +1,16 @@
+package id.ac.ubaya.informatika.foodrecipes_160419075.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ac.ubaya.informatika.foodrecipes_160419075.R
+
+class BagFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bag, container, false)
+    }
+}
